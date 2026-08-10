f8a52d249f712e36c0aeb8045c04cd863b45c304
Diana Osano <dianaosano2007@gmail.com>
2026-08-10 16:38:37 +0800

    Add bool.cs

---
+++ bool.cs
@@ -0,0 +1,11 @@
+using System;
+class Program{
+    static void Main(){
+        Console.WriteLine("Enter the radius of the circle:");
+        double radius = Convert.ToDouble(Console.ReadLine());
+
+        double area = Math.PI * radius * radius;
+
+        Console.WriteLine($"The area of the circle is: {area}");
+    }
+}
