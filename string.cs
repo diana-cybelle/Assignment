b841ef8e2f174dc874ca0a5a6a4640dd4b1a8212
Diana Osano <dianaosano2007@gmail.com>
2026-08-10 16:49:19 +0800

    Add string.cs

---
+++ string.cs
@@ -0,0 +1,12 @@
+using System;
+class Program{
+    static void Main(){
+        Console.Write("Enter your First Name: ");
+        string first = Console.ReadLine();
+
+        Console.Write("Enter your Last Name: ");
+        string second = Console.ReadLine();
+
+        Console.WriteLine(first + second);
+    }
+}
