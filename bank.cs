4aaa26418bc60d0b3022b105fd4abef7644f3e60
Diana Osano <dianaosano2007@gmail.com>
2026-08-10 16:55:48 +0800

    Add bank.cs

---
+++ bank.cs
@@ -0,0 +1,18 @@
+using System;
+class Program{
+    static void Main(){
+
+        Console.Write("Enter principal amount: ");
+        int principalAmount = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter rate of interest: ");
+        int rate = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter time in years: ");
+        int years = int.Parse(Console.ReadLine());
+
+        int interest = ((principalAmount * rate * years)/100);
+
+        Console.Write("Simple Interest " + interest);
+    }
+}
